dcc3df0428197a07cbaec84c226a4a36da9d6ba3
Amitkumbhar5 <amitkumbhar2023@gmail.com>
2023-10-06 19:34:16 +0530

    minor changes

--- stopWatch/app.js
+++ stopWatch/app.js
@@ -3,33 +3,33 @@ const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const reset = document.querySelector('#reset');
 let intv;
-start.addEventListener('click',()=>{
-    const date = new Date();
-    start.setAttribute('disabled','');
-    intv = setInterval(()=>{
-        const latestdate = new Date();
-        const time = new Date(latestdate-date);
-        const sec = time.getUTCSeconds().toString().padStart(2,'0');
-        const min = time.getUTCMinutes().toString().padStart(2,'0');
-        const hou = time.getUTCHours().toString().padStart(2,'0');
-        clk.innerHTML = `${hou}:${min}:${sec}`
-    },1000);
-})
 
-stop.addEventListener('click',(e)=>{
-    clearInterval(intv);
-    start.removeAttribute('disabled');
-})
+// Format the time for display
+function formatTime(date) {
+  const sec = date.getUTCSeconds().toString().padStart(2, '0');
+  const min = date.getUTCMinutes().toString().padStart(2, '0');
+  const hou = date.getUTCHours().toString().padStart(2, '0');
+  return `${hou}:${min}:${sec}`;
+}
 
-reset.addEventListener('click',()=>{
-    clearInterval(intv);
-    start.removeAttribute('disabled');
-    clk.innerHTML = `00:00:00`
-})
+start.addEventListener('click', () => {
+  const date = new Date();
+  start.setAttribute('disabled', '');
+  intv = setInterval(() => {
+    const latestdate = new Date();
+    const time = new Date(latestdate - date);
+    document.title = formatTime(time);
+    clk.innerText = formatTime(time);
+  }, 1000);
+});
 
+stop.addEventListener('click', () => {
+  clearInterval(intv);
+  start.removeAttribute('disabled');
+});
 
-// setInterval(()=>{
-//     const date = new Date 
-//     const time = date.toLocaleTimeString()
-//     clk.innerHTML = time;
-// },1000)
+reset.addEventListener('click', () => {
+  clearInterval(intv);
+  start.removeAttribute('disabled');
+  clk.innerText = '00:00:00';
+});
