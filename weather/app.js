0b9334b53510d50685cb4d0e2c1115d12b626b03
Amitkumbhar5 <amitkumbhar2023@gmail.com>
2023-10-08 15:21:43 +0530

    tailwind frontend

--- weather/app.js
+++ weather/app.js
@@ -1,4 +1,4 @@
-const api_key = process.env.API_KEY;
+const api_key = '997c11233d4b00bea78a47d69e09c75a';
 
 const temp = document.querySelector('.temp')
 const humidity = document.querySelector('.humidity')
@@ -7,7 +7,10 @@ const cityName = document.querySelector('.city')
 const searchBar = document.querySelector('.search input')
 const searchBtn = document.querySelector('.search button')
 const icon = document.querySelector('.weather-icon')
-
+const weather = document.querySelector('.weather')
+const error = document.querySelector('.error')
+weather.style.display = 'none';
+error.style.display = 'none';
 
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${api_key}&units=metric`
 
@@ -17,17 +20,17 @@ const checkWeather = async (city) => {
 
     if(res.status==404)
     {
-        document.querySelector('.error').style.display = 'block';
-        document.querySelector('.weather').style.display = 'none';
+        error.style.display = 'block';
+        weather.style.display = 'none';
     }
     else
     {
-        temp.innerHTML = Math.round(data.main.temp) +'°C'
-        humidity.innerHTML = data.main.humidity;
-        wind.innerHTML = data.wind.speed + ' km/h'
-        cityName.innerHTML = data.name
-        document.querySelector('.error').style.display = 'none';
-        document.querySelector('.weather').style.display='block'
+        temp.innerText = Math.round(data.main.temp) +'°C'
+        humidity.innerText = data.main.humidity +'%';
+        wind.innerText = data.wind.speed + ' km/h'
+        cityName.innerText = data.name
+        error.style.display = 'none';
+        weather.style.display='block'
     
         if(data.weather[0].main == "Clouds")
         {
---
+++ weather/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans'], // 'Your Selected Font' is the name of the font
+      },
+    },
+  },
+  plugins: [],
+}
+
